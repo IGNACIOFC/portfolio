f749b056d854098030111ee7e7af05e374b578dd
IGNACIOFC <45830117+IGNACIOFC@users.noreply.github.com>
2022-03-15 10:33:48 +0100

    import font

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+  <>
+  <Head>
+    <link
+      href="../styles/font/stylesheet.css"
+      rel="stylesheet"
+    />
+  </Head>
+    <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
